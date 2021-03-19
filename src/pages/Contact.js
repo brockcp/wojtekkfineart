d7accafcf4507cded7a80ca4186d38b03af9eff7
brock patterson <brockpatterson@gmail.com>
2021-03-18 19:33:45 -0700

    update & npm

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact(){
     document.title = "Wojtek Kosmowski Fine Art | Contact"
     return (
-       <div className="mh-600 col-sm-12 col-md-10 offset-md-1 col-lg-8 offset-md-2 text-center">
+       <div className="mh-600 col-sm-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 text-center">
          <p className="">Whether you are interested in purchasing a piece of art or learning more about
            my works, please feel free to send me an email.
          </p>
