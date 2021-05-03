49e253f006bd94fd067f72a11fe64cde7c6a56bb
brock patterson <brockpatterson@gmail.com>
2021-05-03 10:17:46 -0700

    removed react-ga

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './style-bootstrap5.css';
 import './style.css'
-import ReactGA from 'react-ga';
-ReactGA.initialize('35207054');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 render((
   <BrowserRouter>
